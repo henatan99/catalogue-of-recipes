5b9e4b645e6ed6e5b77ddd6343bb7226501f35ea
Henok <henatan99@gmail.com>
2021-06-17 01:50:44 +0300

    Add NavBar component apply initial styling and render it in App component

---
+++ src/components/navBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBar = () => (
+  <nav className="nav-bar">
+    <ul className="nav-items">
+      <li className="nav-item">
+        <span className="iconify" data-icon="carbon:user-avatar-filled-alt" data-inline="false" />
+      </li>
+      <li className="nav-item">nomnom</li>
+    </ul>
+  </nav>
+);
+
+export default NavBar;

d5967996b37e191bf353aeaed1bf10253c36b850
Henok <henatan99@gmail.com>
2021-06-17 20:59:44 +0300

    Add Link to Home Page in the NavBar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => (
   <nav className="nav-bar">
     <ul className="nav-items">
+      <li>
+        <Link to="/">
+          Home
+        </Link>
+      </li>
+      <li className="nav-item">nomnom</li>
       <li className="nav-item">
         <span className="iconify" data-icon="carbon:user-avatar-filled-alt" data-inline="false" />
       </li>
-      <li className="nav-item">nomnom</li>
     </ul>
   </nav>
 );

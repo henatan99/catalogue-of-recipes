a5d7a8232f6b2561f876d1f5e2869441c3f84cd4
Henok <henatan99@gmail.com>
2021-06-17 20:25:31 +0300

    Add countLikes action to actions which counts the number of likes on a meal

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,1 +1,2 @@
 export const CHANGE_FILTER = 'CHANGE_FILTER';
+export const COUNT_LIKES = 'COUNT_LIKES';
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,11 @@
-import { CHANGE_FILTER } from './actionTypes';
+import { CHANGE_FILTER, COUNT_LIKES } from './actionTypes';
 
 export const changeFilter = (category) => ({
     type: CHANGE_FILTER,
     category,
 });
+
+export const countLikes = (meal) => ({
+    type: COUNT_LIKES,
+    meal,
+})

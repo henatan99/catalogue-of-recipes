f47df1056823fc644348446a27e07fc639985dbe
Henok <henatan99@gmail.com>
2021-06-17 19:15:48 +0300

    Add categories, filter and changeFilter props to stateful component Catalogue

--- src/components/category.js
+++ src/components/category.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Category = ( {catgoryImage, catageoryName, likes} ) => (
+const Category = ( {catgoryImage, catageoryName, recipes} ) => (
   <div className="category-div">
-    <img className="category-img" alt="category-img" />
+    <img className="category-img" src={catgoryImage} alt="category-img"/>
     <h2 className="recipe">{catageoryName}</h2>
-    <span className="recipe-num">{likes} recipes</span>
+    <span className="recipe-num">{recipes} recipes</span>
   </div>
 );
 
 Category.propTypes = {
   catgoryImage: PropTypes.objectOf(PropTypes.any).isRequired,
   catageoryName: PropTypes.string.isRequired,
-  likes: PropTypes.number.isRequired
+  recipes: PropTypes.number.isRequired
 };
 
 export default Category;
--- src/containers/catalogue.js
+++ src/containers/catalogue.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Category from '../components/category';
 import SearchForm from './searchForm';
 
-const Catalogue = () => (
-  <div>
-    <SearchForm />
-    <Category />
-  </div>
-);
+const Catalogue = ( {categories, filter, changeFilter} ) => {
+  const filterChange = ({ target: { value } }) => changeFilter(value); 
+  const { categoryName, categoryImage, recipes} = categories;
+  
+  return (
+    <div>
+      <SearchForm handleFilterChange={(e) => { filterChange(e); }}/>
+      <Category />
+    </div>
+  )
+};
 
 export default Catalogue;

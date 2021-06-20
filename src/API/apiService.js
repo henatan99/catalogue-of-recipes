019bbb31385be786a5069a82a05693c9f824cf37
Henok <henatan99@gmail.com>
2021-06-20 18:47:52 +0300

    Implement apiService class

---
+++ src/API/apiService.js
@@ -0,0 +1,17 @@
+import http from './baseHttp';
+
+class recipesApiService {
+    getAll() {
+        return http.get('/categories.php');
+    }
+    
+    get(id) {
+        return http.get(`/lookup.php?i=${id}`);
+    }
+
+    findByName(name) {
+        return http.get(`/search.php?s=${name}`)
+    }
+}
+
+export default new recipesApiService

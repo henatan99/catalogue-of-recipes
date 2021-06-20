1561159bff952df59c6ef5b3cf349bde1a0e950c
Henok <henatan99@gmail.com>
2021-06-20 16:45:34 +0300

    Add initialState to store

--- src/reducers/categories.js
+++ src/reducers/categories.js
@@ -2,8 +2,12 @@ import { CHANGE_FILTER } from '../actions/actionTypes';
 
 export default function (state = 'All', action) {
   switch (action.type) {
-    case CHANGE_FILTER:
-      return action.category;
+    case FETCH_CATEGORIES:
+      loading: true;
+    case FETCH_CATEGORIES_SUCCESS:
+      loading: false
+      categories: data
+    case FETCH_CATEGORIES_FAILURE:
     default:
       return state;
   }
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,12 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers';
 
+const initialState = {
+  loading: false,
+  categories: data,
+  error: '',
+}
+
 export default function configureStore(initialState) {
   return createStore(rootReducer, initialState);
 }
